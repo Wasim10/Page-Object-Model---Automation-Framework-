bb57e9db9d225b185909d68077a7ac60268285b4
Wasim10 <mw82777@gmail.com>
2025-08-04 15:30:38 +0500

    first commit

---
+++ src/test/java/testcases/DashbaordTestCase.java
@@ -0,0 +1,29 @@
+package testcases;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pages.BaseTest;
+import pages.DashboardPage;
+import pages.LoginPage;
+import utility.ReadExcelFile;
+
+public class DashbaordTestCase extends BaseTest {
+
+
+	    String fileName = System.getProperty("user.dir") + "\\TestData\\TestPortalData.xlsx";
+
+	    @Test(priority = 1)
+	    void testcourses() {
+	        LoginPage lp = new LoginPage(driver);
+	        String username = ReadExcelFile.getCellValue(fileName, "LoginData", 1, 0);
+	        String password = ReadExcelFile.getCellValue(fileName, "LoginData", 1, 1);
+	        lp.loginToPortal(username, password);
+
+	        DashboardPage dp = new DashboardPage(driver);
+
+	        dp.dashboardClick();
+	    }
+}
